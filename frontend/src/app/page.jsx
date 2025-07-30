8462c86a5a041da26b7bc53b363b19dea7aac130
Umer gull <mohammadmoavia26.com>
2025-07-30 09:41:12 +0500

    Fix homepage redirect logic to check if user already signed up

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -13,19 +13,11 @@ import Reviews from "@/component/Reviews/Reviews";
 import Newsletter from "@/component/News/News";
 import Footer from "@/component/Footer/Footer";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+
 
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/signup');
-    }, 10000); // 10 seconds
 
-    return () => clearTimeout(timer);
-  }, [router]);
   return (
     <div>
       <TopHeader />
